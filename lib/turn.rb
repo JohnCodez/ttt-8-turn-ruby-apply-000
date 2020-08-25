407a8376e750d0cef1c649545e49b80e4f171416
John Exarhouleas <exarhouleasjohn@gmail.com>
2020-08-25 20:48:15 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -43,7 +43,7 @@ def turn(board)
   loop do 
      puts "Please enter 1-9:"
      index = input_to_index(gets.strip)
-    if valid_move?(board, index) = true
+    if valid_move?(board, index) 
       move(board, index)
        break
     end
